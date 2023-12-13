53eb99ab6b4be3beb0954b088598981f2016dacf
“fco-franco” <fcofranco@hotmail.com>
2023-12-13 10:31:13 -0300

    franco calcula

---
+++ sm-aula08/calculadora.py
@@ -0,0 +1,13 @@
+def soma(num1,num2):
+    return num1 + num2
+
+def multip(num1,num2):
+    return num1 * num2
+
+def subtr(num1,num2):
+    return num1 - num2
+
+def div(num1,num2):
+    return num1 / num2
+
+print(div(12,3))
