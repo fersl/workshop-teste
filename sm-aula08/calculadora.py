cb7590390fd40614217202fabe864f6e77fc9076
lucianaPaiva <luciana@vutus.com>
2023-12-13 10:35:46 -0300

    Calculadora Luciana

---
+++ sm-aula08/calculadora.py
@@ -0,0 +1,12 @@
+#função calculadora Luciana IMC
+
+def calculadora_simples(num1, num2):
+    return num1 + num2
+
+# Exemplo de uso:
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+resultado = calculadora_simples(numero1, numero2)
+    
+print(f"Resultado: {resultado}")
